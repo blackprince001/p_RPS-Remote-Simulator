f08b204ffa5b39408b2206f54add99788d779183
Blackprincex <appiahboaduprince@gmail.com>
2022-12-14 12:44:31 +0000

    chore: Game table to engine
    chore: add more features to User Table

--- rps_remote_simulator/database/models.py
+++ rps_remote_simulator/database/models.py
@@ -1,12 +1,27 @@
-from sqlalchemy import Column, String, DateTime, Integer
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, DateTime, Integer, JSON, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
+
+class Game(Base):
+    __tablename__ = "games"
+
+    user_id = Column(ForeignKey("user.id"), primary_key=True)
+    game_result = Column(JSON, nullable=False)
+
+    user = relationship("User", back_populates="gameplays", lazy="selectin")
+
+
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     username = Column(String, nullable=False, unique=True)
     date_created = Column(DateTime, nullable=False)
-    
+
+    is_admin = Column(Boolean, nullable=False, default=True)
+
+    gameplays: list[Game] = relationship(
+        "Game", back_populates="user", lazy="selectin"
+    )
