7328635ecc44cdc94ae8d17376698afa4bbfb54c
Blackprincex <appiahboaduprince@gmail.com>
2022-12-21 08:09:18 +0000

    chore: add admincreate post to endpoint

--- routers/user_end.py
+++ routers/user_end.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 from typing import List
-from schemas.user import UserCreate
+from schemas.user import UserCreate, AdminCreate
 from rps_remote_simulator.database.models import User as UserModel
 from utils.utils import get_db
 
@@ -11,18 +11,19 @@ user = FastAPI()
 
 @user.get("/api/v1/users", tags=["users"])
 async def get_users(db: Session = Depends(get_db)) -> List[UserModel] | None:
-    return db.scalars(select(UserModel).where(UserModel.is_deleted is False)).all()
+    return db.scalars(select(UserModel)).all()
 
 
-@user.get("/api/v1/user/{user_id}", tags=["users"])
-async def get_user(user_id: int, db: Session = Depends(get_db)) -> UserModel | None:
+@user.get("/api/v1/users/{user_id}", tags=["users"])
+async def get_user(user_id: int, db: Session = Depends(get_db)) -> UserModel:
     db_user = db.get(UserModel, user_id)
 
     if db_user.is_deleted is True:
-        return db_user
+        # handle this exception well too
+        raise Exception("User not found!")
+        
+    return db_user
 
-    # handle this exception well too
-    raise Exception("User not found!")
 
 
 @user.post("/api/v1/users", tags=["users"])
@@ -36,7 +37,18 @@ async def create_user(
     return db_user
 
 
-@user.delete("/api/v1/user/{user_id}", tags=["users"])
+@user.post("/api/v1/admins", tags=["users"])
+async def create_admin(
+    new_admin: AdminCreate, db: Session = Depends(get_db)
+) -> UserModel | None:
+    db_admin = UserModel(**new_admin.dict())
+    db.add(db_admin)
+    db.commit()
+    db.refresh(db_admin)
+    return db_admin
+
+
+@user.delete("/api/v1/users/{user_id}", tags=["users"])
 async def delete_user(user_id: int, db: Session = Depends(get_db)) -> None:
     db_user = get_user(db, user_id)
 
