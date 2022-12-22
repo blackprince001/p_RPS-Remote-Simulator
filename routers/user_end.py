959dc842feae08d5d8246fd8b455bc075b1bdd07
Blackprincex <appiahboaduprince@gmail.com>
2022-12-22 22:12:53 +0000

    upd: revoke admin privileges from user endpoint

--- routers/user_end.py
+++ routers/user_end.py
@@ -1,29 +1,10 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from sqlalchemy import select
-from typing import List
-from schemas.user import UserCreate, AdminCreate
+from schemas.user import UserCreate
 from rps_remote_simulator.database.models import User as UserModel
 from utils.utils import get_db
 
 user = FastAPI()
-
-
-@user.get("/api/v1/users", tags=["users"])
-async def get_users(db: Session = Depends(get_db)) -> List[UserModel] | None:
-    return db.scalars(select(UserModel)).all()
-
-
-@user.get("/api/v1/users/{user_id}", tags=["users"])
-async def get_user(user_id: int, db: Session = Depends(get_db)) -> UserModel:
-    db_user = db.get(UserModel, user_id)
-
-    if db_user.is_deleted is True:
-        # handle this exception well too
-        raise Exception("User not found!")
-        
-    return db_user
-
 
 
 @user.post("/api/v1/users", tags=["users"])
@@ -37,21 +18,21 @@ async def create_user(
     return db_user
 
 
-@user.post("/api/v1/admins", tags=["users"])
-async def create_admin(
-    new_admin: AdminCreate, db: Session = Depends(get_db)
-) -> UserModel | None:
-    db_admin = UserModel(**new_admin.dict())
-    db.add(db_admin)
-    db.commit()
-    db.refresh(db_admin)
-    return db_admin
-
-
-@user.delete("/api/v1/users/{user_id}", tags=["users"])
-async def delete_user(user_id: int, db: Session = Depends(get_db)) -> None:
-    db_user = get_user(db, user_id)
+@user.get("/api/v1/users/{user_id}", tags=["users"])
+async def get_user(user_id: int, db: Session = Depends(get_db)) -> UserModel | None:
+    db_user = db.get(UserModel, user_id)
 
     if db_user.is_deleted is True:
-        # change this exception and handle it well with HTTPExceptions
-        raise Exception("This user account has already been deleted!")
+        # handle this exception well too
+        raise Exception("User not found!")
+
+    return db_user
+
+
+@user.put("/api/v1/users", tags=["users"])
+async def change_username(
+    user_id: int, new_username: str, db: Session = Depends(get_db)
+) -> None:
+    db_user = db.get(UserModel, user_id)
+    db_user.username = new_username
+    # updating query goes here
