cd298b14f8a8f9d61af5b80b72418aa08739ea51
Blackprincex <appiahboaduprince@gmail.com>
2022-12-23 04:35:37 +0000

    chore: add get all games from non-deleted users from database

--- routers/admin_end.py
+++ routers/admin_end.py
@@ -3,23 +3,23 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from typing import List
 from schemas.user import AdminCreate
-from rps_remote_simulator.database.models import User as UserModel
+from rps_remote_simulator.database.models import User as UserModel, Game as GameModel
 from utils.utils import get_db
 
 admin = FastAPI()
 
 
-@admin.get("/api/v1/users", tags=["admins"])
+@admin.get("/api/v1/admin/users", tags=["admins"])
 async def get_users(db: Session = Depends(get_db)) -> List[UserModel] | None:
     return db.scalars(select(UserModel).where(UserModel.is_deleted is False)).all()
 
 
-@admin.get("/api/v1/users/deleted", tags=["admins"])
+@admin.get("/api/v1/admin/users/deleted", tags=["admins"])
 async def get_deleted_users(db: Session = Depends(get_db)) -> List[UserModel] | None:
     return db.scalars(select(UserModel).where(UserModel.is_deleted is True)).all()
 
 
-@admin.get("/api/v1/users/{user_id}", tags=["admins"])
+@admin.get("/api/v1/admin/user/{user_id}", tags=["admins"])
 async def get_user(user_id: int, db: Session = Depends(get_db)) -> UserModel | None:
     db_user = db.get(UserModel, user_id)
 
@@ -30,7 +30,16 @@ async def get_user(user_id: int, db: Session = Depends(get_db)) -> UserModel | N
     return db_user
 
 
-@admin.post("/api/v1/admins", tags=["admins"])
+@admin.delete("/api/v1/admin/user/{user_id}", tags=["admins"])
+async def delete_user(user_id: int, db: Session = Depends(get_db)) -> None:
+    db_user = get_user(db, user_id)
+
+    if db_user.is_deleted is True:
+        # change this exception and handle it well with HTTPExceptions
+        raise Exception("This user account has already been deleted!")
+
+
+@admin.post("/api/v1/admin/create", tags=["admins"])
 async def create_admin(
     new_admin: AdminCreate, db: Session = Depends(get_db)
 ) -> UserModel | None:
@@ -41,7 +50,7 @@ async def create_admin(
     return db_admin
 
 
-@admin.get("/api/v1/admins/{admin_id}", tags=["admins"])
+@admin.get("/api/v1/admin/{admin_id}", tags=["admins"])
 async def get_admin(admin_id: int, db: Session = Depends(get_db)) -> UserModel | None:
     db_admin = db.get(UserModel, admin_id)
 
@@ -65,10 +74,11 @@ async def get_deleted_admins(db: Session = Depends(get_db)) -> List[UserModel] |
     ).all()
 
 
-@admin.delete("/api/v1/users/{user_id}", tags=["admins"])
-async def delete_user(user_id: int, db: Session = Depends(get_db)) -> None:
-    db_user = get_user(db, user_id)
-
-    if db_user.is_deleted is True:
-        # change this exception and handle it well with HTTPExceptions
-        raise Exception("This user account has already been deleted!")
+@admin.get("/api/v1/admin/games", tags=["admins"])
+async def get_all_games(db: Session = Depends(get_db)):
+    return db.scalars(
+        select(UserModel)
+        .where(UserModel.is_deleted is False)
+        .select(GameModel)
+        .where(GameModel.user_id == UserModel.id)
+    ).all()
