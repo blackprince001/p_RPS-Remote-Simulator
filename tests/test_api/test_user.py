7a904572997531d96282b5ffe38713474a3ed094
blackprince001 <appiahboaduprince@gmail.com>
2023-01-03 12:33:16 +0000

    (tests) upd: add test_user tests for post and get responses only

--- tests/test_api/test_user.py
+++ tests/test_api/test_user.py
@@ -1,18 +1,25 @@
-from routers.user_end import user
+from routers.user_end import user as usr
 from fastapi.testclient import TestClient
 
-client = TestClient(user)
+client = TestClient(usr)
 
 
 def test_create_user(user):
     new_user_payload = user.to_dict()
 
-    response = client.post(url="/api/v1/user", json=new_user_payload)
+    user_create_response = client.post(url="/api/v1/user", json=new_user_payload)
 
-    assert response.status_code == 200
-    assert response.json()
+    assert user_create_response.status_code == 200
+    assert user_create_response.json()
+    assert user_create_response.json()["is_admin"] == False
 
-def test_read_user_details():
+def test_read_user_details(user):
+    new_user_payload = user.to_dict()
+    create_user = client.post(url="/api/v1/user", json=new_user_payload)
+    
+    # assert if the user has been created first before we read from user_data
+    assert create_user.status_code == 202
+
     old_user_dat_response = client.get(url="/api/v1/user/1")
 
     assert old_user_dat_response.status_code == 200
