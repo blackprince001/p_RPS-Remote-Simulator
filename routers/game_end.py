7f2a7bfca8133f7e1466eb5b243a56cd464b2e10
blackprince001 <appiahboaduprince@gmail.com>
2023-01-14 09:04:51 +0000

    upd: handle deleted_user play game requests and restrict options handling included

--- routers/game_end.py
+++ routers/game_end.py
@@ -3,6 +3,7 @@ from utils.utils import get_db
 from schemas.game import GameplayInit
 from rps_remote_simulator.database.models import Game as GameModel, User as UserModel
 from rps_remote_simulator.game import Gameplay
+from rps_remote_simulator.errors import DeletedUserWarning, IncorrectPlay
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 from typing import List
@@ -15,9 +16,16 @@ game = FastAPI()
 async def create_game(
     choice: str, user_id: int, db: Session = Depends(get_db)
 ) -> GameModel | None:
-    # you can handle the choice if it is correct
-    # before you continue with the game schema
-    # same goes with the user_id
+
+    if choice not in Gameplay.OPTIONS:
+        raise IncorrectPlay(status_code=400, 
+                        details="{choice} is not a move, stick playing the options!")
+    
+    db_user = db.get(UserModel, user_id)
+    
+    if db_user.is_deleted is True:
+        raise DeletedUserWarning(status_code=404, details="User does not exist!")
+
     new_game = GameplayInit(
         user_id=user_id,
         game_result=Gameplay.play(play=choice),
@@ -35,12 +43,12 @@ async def create_game(
 async def get_user_games(
     user_id: int, offset=0, limit=100, db: Session = Depends(get_db)
 ) -> List[GameModel] | None:
-    # remember to check if the user is not deleted or a game is deleted
+
     db_user = db.get(UserModel, user_id)
 
     if db_user.is_deleted is True:
-        # handle this exception well too
-        raise Exception("User not found!")
+        raise DeletedUserWarning(status_code=404, 
+        details="User does not exist! Please register before you can play.")
 
     return db.scalars(
         select(GameModel)
